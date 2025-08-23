094fb794477092f12429bdee53adb5d30845433a
Rami1125 <rami.msarwa1@gmail.com>
2025-08-23 15:43:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxiS3wXwXCyh
 
 // URL של סקריפט Apps Script נפרד לרישום הודעות WhatsApp (⚠️ החלף ב-ID האמיתי של הסקריפט שלך)
 // תצטרך פרויקט Apps Script נפרד שפרוס כיישום אינטרנט במיוחד לרישום הודעות WhatsApp.
-const WHATSAPP_LOG_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyocmflqE_jv0QP__ggyr9A6qB31SBwbUb76bmbZhRHCIpDkl6SxRBplOmb2Cdaah11/exec';
+const WHATSAPP_LOG_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyocmflqE_jv0QP__ggyr9A6qB31SBwbUb76bmbZhRHCIpDkl6SxRBplOmb2Cdaah11/execc';
 
 // URL של סקריפט Apps Script נפרד לשליחת מיילים (⚠️ החלף ב-ID האמיתי של הסקריפט שלך)
 // תצטרך פרויקט Apps Script נפרד נוסף שפרוס כיישום אינטרנט במיוחד לשליחת מיילים.
